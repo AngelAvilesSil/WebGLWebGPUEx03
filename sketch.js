e971e198ada4731fdfc943529822245eb4ecef76
AngelAvilesSil <aavilessil1750@conestogac.on.ca>
2024-11-24 14:37:14 -0500

    The sun looks nice as well as the starfields

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ const kMinCOlor = 150;
 const kMaxColor = 255;
 // Stars should be points with a size of 3.
 const kStarStroke = 3;
+const kSunRadius = 30;
 
 // This is the number of stars that
 // should be in the starfield if non
@@ -32,6 +33,7 @@ let moon;
 let theEarthTexture;
 let theMoonTexture;
 
+
 function preload() {
 	// Will load straightforwardely the two texture images
 	// that I downloaded from where the assignment info and
@@ -39,6 +41,7 @@ function preload() {
 	theEarthTexture = loadImage("assets/textures/earth_daymap.jpg");
 	theMoonTexture = loadImage("assets/textures/moon.jpg")
 }
+
 
 function setup() {
 	createCanvas(700, 700, WEBGL);
@@ -48,12 +51,25 @@ function setup() {
 	setupStarField();
 }
 
+
 function draw() {
 	background(0);
+	lights(180);
+
+	orbitControl();
 
 	// The background of the application should be
 	// filled with (by default) 100 randomly placed stars.
 	drawStarField();
+
+	// At the center of your solar system will be the sun.
+	push();
+		// This will be represented by a yellow sphere.
+		ambientMaterial('yellow');
+		// This will be represented by a yellow sphere.
+		sphere(kSunRadius);
+	pop();
+
 }
 
 
@@ -65,7 +81,7 @@ function setupStarField() {
 			// randomly placed stars
 			x: random(-width / 2, width / 2),		// I was forgetting the center
 			y: random(-height / 2, height / 2),		// the fact about the center in the
-			y: random(-kRandomDeep, kRandomDeep),	// center, so it should be all / 2
+			z: random(-kRandomDeep, kRandomDeep),	// center, so it should be all / 2
 			color: color(
 				random(kMinCOlor, kMaxColor),		// Was leaving it from 0 to 255 but
 				random(kMinCOlor, kMaxColor),		// it was so acid-colorful polybious like
@@ -78,12 +94,12 @@ function setupStarField() {
 function drawStarField() {
 	stars.forEach(star => {
 		push();
-		// These can be white, or you can 
-		// add a little colour to mix things up
-		stroke(star.color);
-		// These should be points with a size of 3.
-		strokeWeight(kStarStroke);
-		point(star.x, star.y, star.z)
+			// These can be white, or you can 
+			// add a little colour to mix things up
+			stroke(star.color);
+			// These should be points with a size of 3.
+			strokeWeight(kStarStroke);
+			point(star.x, star.y, star.z)
 		pop();
 	});
 }
