2db9ec20c9975cec6084a364b9be796291357635
AngelAvilesSil <aavilessil1750@conestogac.on.ca>
2024-11-25 13:05:57 -0500

    The starfield now surround the solar system

--- sketch.js
+++ sketch.js
@@ -15,7 +15,8 @@ const kNumDefaultStars = 100;
 // all looked flat and even
 // in ther same plane, so decided
 // to add some deepness for starfield
-const kRandomDeep = 600;	
+const kRandomDeep = 1400;
+const kInnerDeep = 800;
 
 // Will do the same array handlintg
 // I did in previous assignment with
@@ -64,6 +65,7 @@ function draw() {
 
 	// At the center of your solar system will be the sun.
 	push();
+		noStroke();
 		// This will be represented by a yellow sphere.
 		ambientMaterial('yellow');
 		// This will be represented by a yellow sphere.
@@ -77,11 +79,31 @@ function setupStarField() {
 	// The background of the application should be filled
 	// with (by default) 100 randomly placed stars.
 	for (let i = 0; i < kNumDefaultStars; i++) {
+		let tempX;		// having a temporary X coordinate
+		let tempY;		// having a temporary y coordinate
+		let tempZ;		// having a temporary z coordinate
+		let validDistance = false;		// cheecking if the full pont locatioon is acceptable
+
+		// Decided to do this because persnallyI did not liked that 
+		// there were starts spawning in between the planets, and that
+		// makes abosolutey no sense. so this hould make them exists
+		// around the solar sistem in a radius which I randomly decided
+		// would be safe for showing
+		while (!validDistance) {
+			tempX = random(-kRandomDeep, kRandomDeep);
+			tempY = random(-kRandomDeep, kInnerDeep);
+			tempZ = random(-kRandomDeep, kInnerDeep);
+
+			if (dist(tempX, tempY, tempZ, 0, 0, 0) >= kInnerDeep) {
+				validDistance = true;
+			}
+		}
+
 		stars.push({
 			// randomly placed stars
-			x: random(-width / 2, width / 2),		// I was forgetting the center
-			y: random(-height / 2, height / 2),		// the fact about the center in the
-			z: random(-kRandomDeep, kRandomDeep),	// center, so it should be all / 2
+			x: tempX,
+			y: tempY,
+			z: tempZ,
 			color: color(
 				random(kMinCOlor, kMaxColor),		// Was leaving it from 0 to 255 but
 				random(kMinCOlor, kMaxColor),		// it was so acid-colorful polybious like
