1ae4efa1fd2525670013d127cd085ab0a19d793b
AngelAvilesSil <aavilessil1750@conestogac.on.ca>
2024-11-25 22:43:48 -0500

    All controls are set

--- sketch.js
+++ sketch.js
@@ -7,6 +7,10 @@ const kMaxColor = 255;
 // Stars should be points with a size of 3.
 const kStarStroke = 3;
 const kSunRadius = 30;
+const kMinPlanetSize = 2;
+const kMaxPlanetRotation = 0.2
+const kMinPlanetRotation = 0.01
+const kPlanetRotationStep = 0.01
 
 // This is the number of stars that
 // should be in the starfield if non
@@ -90,10 +94,27 @@ function draw() {
 		//This planet should spin around its own axis.
 		earth.refresh();
 		moon.refresh(earth.x, earth.y);
+		
+		// then lets doo the same for the planets and
+		// their moons if they have
+		planets.forEach(planet => {
+			planet.refresh();
+			if (planet.moon) {
+				planet.moon.refresh(planet.x, planet.y);
+			}
+		});
 	}
 
 	earth.draw();
 	moon.draw();
+
+	// Now need to draw the planets tha are custom
+	planets.forEach(planet => {
+		planet.draw();
+		if (planet.moon) {
+			planet.moon.draw();
+		}
+	});
 
 }
 
@@ -158,7 +179,7 @@ function drawStarField() {
 // noticed that is is (maybe) earier doing it this way rather than with
 // html and other javascritpts APIs... or thats what I hope
 function setAllTheGUI()
- {
+{
 	// A text input for entering the number of stars. This
 	// will need an associated button to take yhe contents
 	// of the text field and update and regenerate the star positions.
@@ -172,8 +193,8 @@ function setAllTheGUI()
 	let howManyStarsButton = createButton('Update Stars');
 	howManyStarsButton.position(800, 40);
 	howManyStarsButton.mousePressed(() => {
-	  numDefaultStars = int(howManyStarsInput.value());
-	  setupStarField();
+		numDefaultStars = int(howManyStarsInput.value());
+		setupStarField();
 	});
 	
 	// A slider controlling the Earth’s distance from the sun.
@@ -191,7 +212,10 @@ function setAllTheGUI()
 	// A slider controlling the Earth’s rotation speed
 	let earthRotationLabel = createP("Earth Rotation Speed:");
 	earthRotationLabel.position(720, 100);
-	let earthRotationSlider = createSlider(0.01, 0.1, earth.orbitSpeed, 0.01);
+	let earthRotationSlider = createSlider(
+		kMinPlanetRotation, kMaxPlanetRotation,
+		earth.orbitSpeed, kPlanetRotationStep
+	);
 	earthRotationSlider.position(720, 140);
 	let earthRotationValueShow = createP(earthRotationSlider.value().toString());
 	earthRotationValueShow.position(900, 125);
@@ -215,7 +239,10 @@ function setAllTheGUI()
 	// A slider controlling the Moon’s rotation speed.
 	let moonRotationLabel = createP("Moon Rotation Speed:");
 	moonRotationLabel.position(720,200);
-	let moonRotationSlider = createSlider(0.02, 0.2, moon.orbitSpeed, 0.02);
+	let moonRotationSlider = createSlider(
+		kMinPlanetRotation, kMaxPlanetRotation,
+		moon.orbitSpeed, kPlanetRotationStep
+	);
 	moonRotationSlider.position(720, 240);
 	let moonRotationValueShow = createP(moonRotationSlider.value().toString());
 	moonRotationValueShow.position(900,225);
@@ -223,4 +250,179 @@ function setAllTheGUI()
 		moon.orbitSpeed = moonRotationSlider.value();
 		moonRotationValueShow.html(moonRotationSlider.value().toString());
 	});
- }
+
+	
+	// Additionally, GUI elements will need to be provided to allow for the
+	// creation of additional planets.
+	// The following should be configurable for additional planets being added:
+
+	// Size of the planet (radius).
+	let customSizePlanetLabel = createP("Custom Planet Size:");
+	customSizePlanetLabel.position(720,250);
+	let customSizePlanetSlider = createSlider(kMinPlanetSize, kSunRadius*0.8, kMinPlanetSize, 1);
+	customSizePlanetSlider.position(720,290);
+	let customPlanetValueShow = createP(customSizePlanetSlider.value().toString());
+	customPlanetValueShow.position(900,275);
+	customSizePlanetSlider.input(() => {
+		customPlanetValueShow.html(customSizePlanetSlider.value().toString());
+	});
+
+	// Colour of the planet
+	let customColorPlanetLabel = createP("Custom Planet Colour:");
+	customColorPlanetLabel.position(950,250);
+	let customCOlorPlanetPicker = createColorPicker('green');
+	customCOlorPlanetPicker.position(950,290);
+
+	// Speed at which the planet moves around the sun.
+	let customPlanetRotationLabel = createP("Custom Planet Rotation Speed:");
+	customPlanetRotationLabel.position(720,300)
+	let customPlanetRotationSlider;
+	if (planets.length > 0) {
+		customPlanetRotationSlider = createSlider(
+			kMinPlanetRotation, kMaxPlanetRotation,
+			planets[planets.length - 1].orbitSpeed, kPlanetRotationStep
+		);
+	} else {
+		customPlanetRotationSlider = createSlider(
+			kMinPlanetRotation, kMaxPlanetRotation,
+			(kMinPlanetRotation + kMaxPlanetRotation) / 2, kPlanetRotationStep
+		);
+	}
+	customPlanetRotationSlider.position(720, 340);
+	let customPlanetRotationValueShow = createP(customPlanetRotationSlider.value().toString());
+	customPlanetRotationValueShow.position(900,325);
+	customPlanetRotationSlider.input(() => {
+		if (planets.length > 0) {
+			planets[planets.length - 1].orbitSpeed = (customPlanetRotationSlider.value());
+		}
+		customPlanetRotationValueShow.html(customPlanetRotationSlider.value().toString());
+	});
+
+	// Distance of the planet from the sun.
+	let customPlanetDistanceLabel = createP("Custom Planet Distance from Sun:");
+	customPlanetDistanceLabel.position(720,350)
+	let customPlanetDistanceSlider;
+	if (planets.length > 0) {
+		customPlanetDistanceSlider = createSlider(
+			kSunRadius * 1.50, kSunRadius * 10.00, planets[planets.length-1].orbitRadius, 1
+		);
+	} else {
+		customPlanetDistanceSlider = createSlider(
+			kSunRadius * 1.50, kSunRadius * 10.00, 
+			(kSunRadius * 1.50 + kSunRadius * 10.00) / 2, 1
+		);
+	}
+	customPlanetDistanceSlider.position(720, 390);
+	let customPlanetDistanceValueShow = createP(customPlanetDistanceSlider.value().toString());
+	customPlanetDistanceValueShow.position(900,375);
+	customPlanetDistanceSlider.input(() => {
+		if (planets.length > 0) {
+			planets[planets.length - 1].orbitRadius = (customPlanetDistanceSlider.value());
+		}
+		customPlanetDistanceValueShow.html(customPlanetDistanceSlider.value().toString());
+	});
+	
+	// Whether or not the planet has a moon (Boolean
+	// value. Each planet can only have one moon).
+	let customPlanetHasMoonLabel = createP("Has Moon:");
+	customPlanetHasMoonLabel.position(720,400);
+	let customPlanetHasMoonBox = createCheckbox(false);
+	customPlanetHasMoonBox.position(875,417);
+
+	// Size of the moon (radius)
+	let customSizePlanetMoonLabel = createP("Custom Moon Size:");
+	customSizePlanetMoonLabel.position(720,430);
+	let customSizePlanetMoonSlider = createSlider(
+		kMinPlanetSize, kSunRadius * 0.8,
+		random(kMinPlanetSize, kSunRadius * 0.8), 1);
+	customSizePlanetMoonSlider.position(720,470);
+	let customPlanetMoonValueShow = createP(customSizePlanetMoonSlider.value().toString());
+	customPlanetMoonValueShow.position(900,455);
+	customSizePlanetMoonSlider.input(() => {
+		customPlanetMoonValueShow.html(customSizePlanetMoonSlider.value().toString());
+	});
+
+	// Distance of the moon from the planet.
+	let customMoonDistanceLabel = createP("Custom Moon Distance from Planet:");
+	customMoonDistanceLabel.position(720,480);
+	let customMoonDistanceSlider;
+	if (planets.length > 0) {
+		customMoonDistanceSlider = createSlider(planets[planets.length - 1].radius * 1.20, planets[planets.length - 1].radius * 3.0, planets[planets.length - 1].moon.orbitRadius, 1);
+	} else {
+		customMoonDistanceSlider = createSlider(
+			earth.radius * 1.20, earth.radius * 3.0, 
+			random(earth.radius * 1.20, earth.radius * 3.0), 1
+		);
+	}
+	customMoonDistanceSlider.position(720, 520);
+	let customMoonDistanceValueShow = createP(customMoonDistanceSlider.value().toString());
+	customMoonDistanceValueShow.position(900,505)
+	customMoonDistanceSlider.input(() => {
+		if (planets.length > 0 && planets[planets.length].moon != null) {
+			planets[planets.length - 1].moon.orbitRadius = customMoonDistanceSlider.value()
+		}
+		customMoonDistanceValueShow.html(customMoonDistanceSlider.value().toString());
+	});
+
+	// Speed at which the moon orbits the planet.
+	let customMoonRotationLabel = createP("Custom Moon Distance from Planet:");
+	customMoonRotationLabel.position(720,530);
+	let customMoonRotationSlider;
+	if (planets.length > 0) {
+		customMoonRotationSlider = createSlider(
+			kMinPlanetRotation, kMaxPlanetRotation,
+			planets[planets.length - 1].moon.orbitSpeed, kPlanetRotationStep
+		);
+	} else {
+		customMoonRotationSlider = createSlider(
+			kMinPlanetRotation, kMaxPlanetRotation,
+			random(kMinPlanetRotation, kMaxPlanetRotation), kPlanetRotationStep
+		);
+	}
+	customMoonRotationSlider.position(720, 570);
+	let customMoonRotationValueShow = createP(customMoonRotationSlider.value().toString());
+	customMoonRotationValueShow.position(900,555)
+	customMoonRotationSlider.input(() => {
+		if (planets.length > 0 && planets[planets.length].moon != null) {
+			planets[planets.length - 1].moon.orbitSpeed = customMoonRotationSlider.value()
+		}
+		customMoonRotationValueShow.html(customMoonRotationSlider.value().toString());
+	});
+
+	// Button for adding planet
+	let addPlanetButton = createButton('Add Planet');
+	addPlanetButton.position(720, 600);
+	addPlanetButton.mousePressed(() => {
+		let tempPlanet = new Planet(
+			customSizePlanetSlider.value(), customPlanetDistanceSlider.value(),
+			customPlanetRotationSlider.value, customCOlorPlanetPicker.value()
+		);
+		if (customPlanetHasMoonBox.value()) {
+			tempPlanet.moon = new Moon(
+				customSizePlanetMoonSlider.value(), customMoonDistanceSlider.value(),
+				customMoonRotationSlider.value(), theMoonTexture
+			);
+		}
+	});
+
+	// Remove last planet
+	let removePlanetButton = createButton('Remove Last Planet');
+	removePlanetButton.position(850, 600);
+	removePlanetButton.mousePressed(() => {
+		planets.pop();
+	});
+
+	// Pause resume button
+	let pauseResumeButton = createButton("Pause/Resume");
+	pauseResumeButton.position(720,630);
+	pauseResumeButton.mousePressed(() => {
+		isPaused = !isPaused
+	});
+
+	// Reset button
+	let resetButton = createButton("Reset");
+	resetButton.position(850,630);
+	resetButton.mousePressed(() => {
+		
+	});
+}
